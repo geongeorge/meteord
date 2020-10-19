a04f22c7d936d8b8871175102c872d7744ed682c
Geon George <geongeorge98@gmail.com>
2020-10-19 22:25:46 +0530

    Enabling a temp line

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -6,6 +6,7 @@ apt-get -y install build-essential g++ libssl-dev git python
 npm install -g node-gyp
 # pre-install node source code for faster building
 echo "Pre-install node source code for faster building v${NODE_VERSION}"
-# $(npm bin -g)/node-gyp install ${NODE_VERSION}
+# TEMP: Enabling this
+$(npm bin -g)/node-gyp install ${NODE_VERSION}
 
 bash $METEORD_DIR/lib/cleanup.sh
