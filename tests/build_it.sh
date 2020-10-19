c49ee17e64cf917e21de03f42dc206960b7526a6
Geon George <geongeorge98@gmail.com>
2020-10-19 20:06:00 +0530

    Docker now adds tags and pushes

--- tests/build_it.sh
+++ tests/build_it.sh
@@ -3,11 +3,11 @@ set -x
 
 : ${NODE_VERSION?"NODE_VERSION has not been set."}
 
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-base" ../base && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-base" abernix/meteord:base
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-onbuild" ../onbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-onbuild" abernix/meteord:onbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-devbuild" ../devbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-devbuild" abernix/meteord:devbuild
-docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "abernix/meteord:node-${NODE_VERSION}-binbuild" ../binbuild && \
-  docker tag "abernix/meteord:node-${NODE_VERSION}-binbuild" abernix/meteord:binbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-base" ../base && \
+  docker tag "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-base" uncleiroh/meteord-puppeteer:base
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-onbuild" ../onbuild && \
+  docker tag "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-onbuild" uncleiroh/meteord-puppeteer:onbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-devbuild" ../devbuild && \
+  docker tag "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-devbuild" uncleiroh/meteord-puppeteer:devbuild
+docker build --build-arg "NODE_VERSION=${NODE_VERSION}" -t "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-binbuild" ../binbuild && \
+  docker tag "uncleiroh/meteord-puppeteer:node-${NODE_VERSION}-binbuild" uncleiroh/meteord-puppeteer:binbuild
