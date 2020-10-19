508e5c3157070b4cacff42f5fe497a9cd1aebc98
Geon George <geongeorge98@gmail.com>
2020-10-19 19:47:49 +0530

    Adds nvm install

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -9,15 +9,28 @@ NODE_ARCH=x64
 
 # check we need to do this or not
 
-NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
+# NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
 
-cd /tmp
-curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
-tar xvzf ${NODE_DIST}.tar.gz
-rm -rf /opt/nodejs
-mv ${NODE_DIST} /opt/nodejs
+# cd /tmp
+# curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
+# tar xvzf ${NODE_DIST}.tar.gz
+# rm -rf /opt/nodejs
+# mv ${NODE_DIST} /opt/nodejs
 
-ln -sf /opt/nodejs/bin/node /usr/bin/node
-ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+# ln -sf /opt/nodejs/bin/node /usr/bin/node
+# ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+
+
+
+echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
+echo ' [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+node -v
+nvm install v12
+nvm alias default v12
+
+echo "Current node version"
+node -v
+
 
 npm install --global npm@6
