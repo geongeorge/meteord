7fd46a8ceb10df7653e9deaca37aaa2fa70748ba
Geon George <geongeorge98@gmail.com>
2020-10-19 21:48:34 +0530

    Check node install

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -9,26 +9,19 @@ NODE_ARCH=x64
 
 # check we need to do this or not
 
-# NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
-
-# cd /tmp
-# curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
-# tar xvzf ${NODE_DIST}.tar.gz
-# rm -rf /opt/nodejs
-# mv ${NODE_DIST} /opt/nodejs
-
-# ln -sf /opt/nodejs/bin/node /usr/bin/node
-# ln -sf /opt/nodejs/bin/npm /usr/bin/npm
+NODE_DIST=node-v${NODE_VERSION}-linux-${NODE_ARCH}
 
 
+cd /tmp
+curl -O -L http://nodejs.org/dist/v${NODE_VERSION}/${NODE_DIST}.tar.gz
+tar xvzf ${NODE_DIST}.tar.gz
+rm -rf /opt/nodejs
+mv ${NODE_DIST} /opt/nodejs
 
-# apt-get install -y build-essential checkinstall libssl-dev
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
+ln -sf /opt/nodejs/bin/node /usr/bin/node
+ln -sf /opt/nodejs/bin/npm /usr/bin/npm
 
 
-
-# nvm install v12
-# nvm alias default node
 
 echo "Current node version"
 node -v
