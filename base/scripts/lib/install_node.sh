36ebff80ffa071af3c23be3f72329295f0004a7c
Geon George <geongeorge98@gmail.com>
2020-10-19 19:56:13 +0530

    Ambegious redirect fix

--- base/scripts/lib/install_node.sh
+++ base/scripts/lib/install_node.sh
@@ -22,8 +22,8 @@ NODE_ARCH=x64
 
 
 
-echo 'export NVM_DIR="/opt/circleci/.nvm"' >> $BASH_ENV
-echo ' [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> $BASH_ENV
+echo 'export NVM_DIR="/opt/circleci/.nvm"' >> "$BASH_ENV"
+echo ' [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"' >> "$BASH_ENV"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.36.0/install.sh | bash
 node -v
 nvm install v12
