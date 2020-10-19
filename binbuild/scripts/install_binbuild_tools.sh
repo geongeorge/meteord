d10808cd0aea255e212ba4d65f0a3db4cf14abb6
Geon George <geongeorge98@gmail.com>
2020-10-19 19:30:00 +0530

    Switch to machine

--- binbuild/scripts/install_binbuild_tools.sh
+++ binbuild/scripts/install_binbuild_tools.sh
@@ -5,6 +5,7 @@ apt-get update -y
 apt-get -y install build-essential g++ libssl-dev git python
 npm install -g node-gyp
 # pre-install node source code for faster building
+echo "Pre-install node source code for faster building v${NODE_VERSION}"
 $(npm bin -g)/node-gyp install ${NODE_VERSION}
 
 bash $METEORD_DIR/lib/cleanup.sh
